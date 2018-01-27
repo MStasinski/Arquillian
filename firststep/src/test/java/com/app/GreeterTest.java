6128db095b53aa2cd0e2c8c309ce736fb7aa661b
Stachu80 <michal.stasinski@poczta.fm>
2018-01-27 13:28:46 +0100

    first step app

---
+++ firststep/src/test/java/com/app/GreeterTest.java
@@ -0,0 +1,33 @@
+package com.app;
+
+import org.jboss.arquillian.container.test.api.Deployment;git status
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+    @Inject
+    private Greeter greeter;
+
+   @Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addClasses(Greeter.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+
+    @Test
+    public void should_create_greeting() {
+        Assert.fail("Not yet implemented");
+    }
+
+}
