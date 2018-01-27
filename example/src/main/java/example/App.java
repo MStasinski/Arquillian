7236d6878d63d54836789e60d40d8c21525ccb4b
Stachu80 <michal.stasinski@poczta.fm>
2018-01-27 14:07:51 +0100

    example

--- example/src/main/java/example/App.java
+++ example/src/main/java/example/App.java
@@ -6,9 +6,6 @@ import java.io.PrintStream;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 
     public void greet(PrintStream to, String name) {
         to.println(createGreeting(name));
