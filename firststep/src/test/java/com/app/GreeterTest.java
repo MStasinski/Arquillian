1cbcbe633f2345f9a873df5ae63a05bbfa6c1711
Stachu80 <michal.stasinski@poczta.fm>
2018-01-27 13:30:53 +0100

    first commit

--- firststep/src/test/java/com/app/GreeterTest.java
+++ firststep/src/test/java/com/app/GreeterTest.java
@@ -1,6 +1,6 @@
 package com.app;
 
-import org.jboss.arquillian.container.test.api.Deployment;git status
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
